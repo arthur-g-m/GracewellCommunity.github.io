8f10340f65f298690fb115633a67abb7e0bb9a6a
arthur-g-m <142493782+arthur-g-m@users.noreply.github.com>
2023-08-21 06:24:49 +0200

    Add files via upload

---
+++ js_files/home_page.js
@@ -0,0 +1,109 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Vue.js code for recent sermons
+    new Vue({
+      el: "#recent-sermons",
+      data: {
+        sermons: [
+          {
+            id: 1,
+            imageSrc: "/GracewellCommunity.github.io/website_images/pexels-pixabay-the-cross.jpg",
+            title: "Sermon Title 1"
+          },
+          {
+            id: 2,
+            imageSrc: "/GracewellCommunity.github.io/website_images/pexels-pixabay-the-cross.jpg",
+            title: "Sermon Title 2"
+          },
+          {
+            id: 3,
+            imageSrc: "/GracewellCommunity.github.io/website_images/pexels-pixabay-the-cross.jpg",
+            title: "Sermon Title 3"
+          },
+          {
+            id: 4,
+            imageSrc: "/GracewellCommunity.github.io/website_images/pexels-pixabay-the-cross.jpg",
+            title: "Sermon Title 4"
+          },
+          {
+            id: 5,
+            imageSrc: "/GracewellCommunity.github.io/website_images/pexels-pixabay-the-cross.jpg",
+            title: "Sermon Title 5"
+          },
+          {
+            id: 6,
+            imageSrc: "/GracewellCommunity.github.io/website_images/pexels-pixabay-the-cross.jpg",
+            title: "Sermon Title 6"
+          },
+          {
+            id: 7,
+            imageSrc: "/GracewellCommunity.github.io/website_images/pexels-pixabay-the-cross.jpg",
+            title: "Sermon Title 7"
+          },
+          {
+            id: 8,
+            imageSrc: "/GracewellCommunity.github.io/website_images/pexels-pixabay-the-cross.jpg",
+            title: "Sermon Title 8"
+          },
+          {
+            id: 9,
+            imageSrc: "/GracewellCommunity.github.io/website_images/pexels-pixabay-the-cross.jpg",
+            title: "Sermon Title 8"
+          },
+        ]
+      }
+    });
+    // Vue.js code for recent events
+    new Vue({
+      el: "#recent-events",
+      data: {
+        events: [
+          {
+            id: 1,
+            imageSrc: "/GracewellCommunity.github.io/website_images/pexels-pixabay-the-cross.jpg",
+            title: "Event Title 1"
+          },
+          {
+            id: 2,
+            imageSrc: "/GracewellCommunity.github.io/website_images/pexels-pixabay-the-cross.jpg",
+            title: "Event Title 2"
+          },
+          {
+            id: 3,
+            imageSrc: "/GracewellCommunity.github.io/website_images/pexels-pixabay-the-cross.jpg",
+            title: "Event Title 3"
+          },
+          {
+            id: 4,
+            imageSrc: "/GracewellCommunity.github.io/website_images/pexels-pixabay-the-cross.jpg",
+            title: "Event Title 1"
+          },
+          {
+            id: 5,
+            imageSrc: "/GracewellCommunity.github.io/website_images/pexels-pixabay-the-cross.jpg",
+            title: "Event Title 2"
+          },
+          {
+            id: 6,
+            imageSrc: "/GracewellCommunity.github.io/website_images/pexels-pixabay-the-cross.jpg",
+            title: "Event Title 3"
+          },
+          {
+            id: 7,
+            imageSrc: "/GracewellCommunity.github.io/website_images/pexels-pixabay-the-cross.jpg",
+            title: "Event Title 1"
+          },
+          {
+            id: 8,
+            imageSrc: "/GracewellCommunity.github.io/website_images/pexels-pixabay-the-cross.jpg",
+            title: "Event Title 2"
+          },
+          {
+            id: 9,
+            imageSrc: "/GracewellCommunity.github.io/website_images/pexels-pixabay-the-cross.jpg",
+            title: "Event Title 3"
+          },
+          // Add more events as needed
+        ]
+      }
+    });
+  });
